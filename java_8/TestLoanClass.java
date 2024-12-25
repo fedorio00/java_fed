c227361c23ab6f8bd3de93d6223a88945dc76131
fedorio00 <Fedor03092002@gmail.com>
2024-12-25 11:12:08 +0300

    итог

--- java_8/TestLoanClass.java
+++ java_8/TestLoanClass.java
@@ -22,6 +22,10 @@ public class TestLoanClass {
                 if (data == 2) {
                     System.out.println("Введите год: ");
                     int year = input.nextInt();
+                    if (year < 1) {
+                        System.out.println("Ошибка: год должен быть больше 0");
+                        return;
+                    }
 
                     if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
                         System.out.println("Високосный год");
